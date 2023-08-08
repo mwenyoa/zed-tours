f02e42d081de7507b45ee35b815a7fb327f11cbe
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-08 13:32:58 +0200

    Mount user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,11 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  getUsersStatics,
 } = require("../controllers/usersController");
 
 // define users routes
+router.route('/users-stats').get(getUsersStatics)
 router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
