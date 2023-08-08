4264d89c07098f0ca15f4110f4086410a1db1b26
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-08 13:35:05 +0200

    Remove Page Not Found Handler

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
 const {
   toursListing,
   tourDetails,
-  pageNotFound,
 } = require("./../controllers/viewsController");
 
 //root route
 router.get("/", toursListing);
-router.get("/tour/:id", tourDetails);
-router.get("*", pageNotFound);
+router.get("/:id", tourDetails);
 
 module.exports = router;
