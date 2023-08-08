851f9683843d6c11d1bb13260c8d8c3cc8a36de5
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-08 13:22:35 +0200

    Add global error class to view router

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel')
+const AppError = require("./../utilities/Error");
 
 exports.toursListing = async (req, res, next) => {
     const tours = await Tour.find() ;
@@ -9,14 +10,21 @@ exports.toursListing = async (req, res, next) => {
   next()
 };
 
-exports.tourDetails = (req, res) => {
+exports.tourDetails = async(req, res, next) => {
+   const tour = Tour.findById(req.query.id);
   res.status(200).render("tour", {
     title: "Tour Detail",
+    tour
   });
+ return next()
 };
 
-exports.pageNotFound = (req, res) => {
-  res.status(404).render("notfound", {
-    title: "Not Found"
-  })
+exports.pageNotFound = (req, res, next) => {
+
+  // res.status(404).render("notfound", {
+  //   title: "Not Found",
+  //   status: 'fail',
+  //   error: `No resource exists for the url ${req.originalUrl}`
+  // })
+  next(new AppError(`No resource exists for the url ${req.originalUrl}`));
 }
