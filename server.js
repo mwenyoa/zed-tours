4ea496097603b86eb4ec32b63aa32333478daa3b
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-08 12:55:11 +0200

    Handle uncaught  exceptions and unhandled promises  rejections

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const mongoose = require('mongoose');
 
+// Handle Uncaught Exceptions
+  process.on('uncaughtException', (err) => {
+    console.log("Uncaught Exception...Server Shutingdown");
+    process.exit(1);
+  })
+
 const DBS = process.env.DB_LOCAL;
 mongoose.connect(DBS).then(() => {
-
+ 
 }).catch((e) => {
   throw new Error(e);
 })
@@ -12,6 +18,15 @@ mongoose.connect(DBS).then(() => {
 const app = require('./app');
 const port = process.env.PORT || 3001;
 // start server
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server waiting for requests on port ${port}`);
+  });
+
+  // Handling Unhandled Promise Rejections
+  process.on('unhandledRejection', (err) =>{
+     console.log(err.name, err.message)
+    //  gracefully shudown server
+     server.close(() =>{
+      process.exit(1);
+     })
   });
