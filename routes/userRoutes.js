ca5ecc5b8ee54e10d46594097a036b25e50bb596
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-09 12:03:23 +0200

    Define user signup and signin routes in the user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,18 @@ const router = express.Router();
 const {
   getUsers,
   getUser,
-  createUser,
   updateUser,
   deleteUser,
   getUsersStatics,
 } = require("../controllers/usersController");
+const { SignUp, SignIn } = require('./../controllers/authController');
 
+// define user auth routes
+ router.post('/signup', SignUp);
+ router.post('/login', SignIn);
 // define users routes
 router.route('/users-stats').get(getUsersStatics)
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getUsers)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
