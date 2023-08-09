734000129fa63b9719eca1a8097c5900ab36794c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-09 12:04:25 +0200

    Encrypt  user password and  create an  instance method for compared passwords during login

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const bycrpt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "User should have a name"],
     minlength: [3, "Usename must not be less than 3 characters"],
-    maxlength: [20, "Username must not be more than 20 characters"]
+    maxlength: [20, "Username must not be more than 20 characters"],
   },
   email: {
     type: String,
@@ -13,30 +14,59 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     required: [true, "User email is already in use"],
     minlength: [10, "Email address must not be less than 10 characters"],
-    maxlength: [40, "Email address must not be more than 40 characters"]
+    maxlength: [40, "Email address must not be more than 40 characters"],
+  },
+  password: {
+    type: String,
+    required: [true, "User account should have a password"],
+    minlength: [6, "User password must be 6 or more characters"],
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Password Confirm field is required"],
+    minlength: [6, "Password confirm must be 6 or more characters"],
+    validate: {
+      validator: function (passwconf) {
+        return passwconf === this.password;
+      },
+      message: "Two passwords did not match",
+    },
+  },
+  photo: {
+    type: String,
+    trim: true,
   },
   age: {
     type: Number,
-    required: [true, "User age is required"],
-    min: [10, "Minimum age required is 10"],
-    max: [100, 'Maximum age limit is 100']
   },
   occupation: {
     type: String,
-    required: [true, "User occupation should be specified"],
-    minlength: [5, "User occupation must not be less than 5 characters"],
-    maxlength: [25, "User occupation must not be more than 25 characters"]
   },
-  created_at:{
+  created_at: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
   },
-  updated_at:{
+  updated_at: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
+  },
+});
+
+// encrypt password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bycrpt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+  } else {
+    return next();
   }
 });
 
+// password compare instance method
+userSchema.methods.comparePasswords =  async function(userPaswword, dbPassword){
+  return await bycrpt.compare(userPaswword, dbPassword);
+}
+
 module.exports = mongoose.model("User", userSchema);
-
-
