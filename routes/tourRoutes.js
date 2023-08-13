e33a67587eecd9982030dc48bef17961610e5944
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-13 22:19:55 +0200

    Protect and restrict access to tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,13 +7,20 @@ const {
   deleteTour,
   updateTour,
   tourStats,
-  topCheapTours
+  topCheapTours,
 } = require("../controllers/toursController");
-const { LoginVerification } = require("./../controllers/authController");
+const {
+  LoginVerification,
+  AuthorizeUser,
+} = require("./../controllers/authController");
 
 router.route("/top-cheap-tours").get(topCheapTours, getTours);
-router.route("/").get(LoginVerification, getTours).post(createTour); 
-router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+router.route("/").get(LoginVerification, getTours).post(createTour);
+router
+  .route("/:id")
+  .get(getTour, LoginVerification)
+  .patch(LoginVerification, AuthorizeUser('tourguider', 'admin'), updateTour)
+  .delete(LoginVerification, AuthorizeUser('tourguider', 'admin'), deleteTour);
 router.route("/statistics").get(tourStats);
 
 module.exports = router;
